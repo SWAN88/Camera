ed45c12909e825cb8aa021d4f5c7b8a01ef832c0
白鳥克哉 <kyo.soc.einstein88@gmail.com>
2017-10-19 20:58:45 +0900

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 白鳥克哉 on 2017/10/18.
+//  Copyright © 2017年 白鳥克哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        
+        let camera = UIImagePickerControllerSourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
